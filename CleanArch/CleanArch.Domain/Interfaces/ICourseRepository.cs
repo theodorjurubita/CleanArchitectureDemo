af7183d644b754c32c951476c1d2ee609ed1c099
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2021-09-03 11:58:08 +0300

    Service and Repository interfaces for Courses + Course View Model

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
