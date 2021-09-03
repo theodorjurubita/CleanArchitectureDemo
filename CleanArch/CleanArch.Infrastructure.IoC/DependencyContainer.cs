e9b9faab2655961c8710d3a73308a4ec3c02ee72
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2021-09-03 15:55:13 +0300

    Added dependecy containter for application and infrastructure layers.

---
+++ CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infrastructure.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArch.Infrastructure.Data.Context;
+using CleanArch.Infrastructure.IoC;
 
 namespace CleanArch.Mvc
 {
@@ -33,6 +34,7 @@ namespace CleanArch.Mvc
             });
             services.AddControllersWithViews();
             services.AddRazorPages();
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +66,11 @@ namespace CleanArch.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
