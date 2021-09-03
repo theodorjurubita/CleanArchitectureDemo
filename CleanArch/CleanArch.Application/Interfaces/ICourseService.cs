55acd77a1e72b6ee2d3308e2fbfcca4c8d8ab8ca
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2021-09-03 15:38:02 +0300

    Commit implementation of service and repository of courses.

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using CleanArch.Application.ViewModels;
+﻿using CleanArch.Application.ViewModels;
 
 namespace CleanArch.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<CourseViewModel> GetCourses();
+        CourseViewModel GetCourses();
     }
 }
---
+++ CleanArch/CleanArch.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infrastructure.Data.Context;
+
+namespace CleanArch.Infrastructure.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _universityDbContext;
+
+        public CourseRepository(UniversityDbContext universityDbContext)
+        {
+            _universityDbContext = universityDbContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _universityDbContext.Courses;
+        }
+    }
+}
