7adb6bd4187e5babb6e694ca4dc8cd2ad8cacb85
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2021-09-06 14:17:24 +0300

    Add Api Project and controller + some updates to Clean Arch Application.

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -5,5 +5,6 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void Create(CourseViewModel courseViewModel);
     }
 }
--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
 using CleanArch.Domain.Interfaces;
+using CleanArchitecture.Domain.Core.Bus;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
 
         public CourseViewModel GetCourses()
@@ -20,6 +23,16 @@ namespace CleanArch.Application.Services
             {
                 Courses = _courseRepository.GetCourses()
             };
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                    courseViewModel.Name,
+                    courseViewModel.Description,
+                    courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
         }
     }
 }
